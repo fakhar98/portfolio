2a176f1ee8e18cabd17a849f02de365255edf434
fakhar98 <fakharkaramat9@gmail.com>
2026-08-01 00:54:54 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
     },
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
